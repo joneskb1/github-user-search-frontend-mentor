0a69b6af1a9fdffae8807c0e74329626b37ede2c
Kayla Jones <kayla3445@yahoo.com>
2022-03-26 10:43:31 -0500

    done

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,5 +1,3 @@
-// work on details icons/text for dark mode reg & NA
-
 const logo = document.querySelector(".logo");
 const lightModeContainer = document.querySelector(".light-mode-container");
 const lightModeText = document.querySelector(".light-mode-text");
@@ -26,7 +24,7 @@ const searchInput = document.querySelector(".username-input");
 const error = document.querySelector(".error");
 const statText = [...document.querySelectorAll(".stat-text")];
 const statNum = [...document.querySelectorAll(".stat-num")];
-const detailText = [...document.querySelectorAll(".detail-text")];
+let nullItemsArr = [];
 
 class FetchWrapper {
   constructor(baseURL) {
@@ -38,8 +36,79 @@ class FetchWrapper {
   }
 }
 
+function getNA(data) {
+  for (const key in data) {
+    if (key === "location" && data[key] === null) {
+      nullItemsArr.push(key);
+    } else if (key === "blog" && data[key] === "") {
+      nullItemsArr.push(key);
+    } else if (key === "twitter_username" && data[key] === null) {
+      nullItemsArr.push(key);
+    } else if (key === "company" && data[key] === null) {
+      nullItemsArr.push(key);
+    } else if (key === "bio" && data[key] === null) {
+      nullItemsArr.push("bio");
+    }
+  }
+
+  return nullItemsArr;
+}
+
+function formatDetails() {
+  // bio
+  if (
+    nullItemsArr.includes("bio") &&
+    lightModeImg.classList.contains("dark-sun")
+  ) {
+    bio.classList.add("not-available-dark");
+    bio.classList.remove("not-available-light");
+  } else if (
+    nullItemsArr.includes("bio") &&
+    !lightModeImg.classList.contains("dark-sun")
+  ) {
+    bio.classList.add("not-available-light");
+    bio.classList.remove("not-available-dark");
+  } else {
+    bio.classList.remove("not-available-light");
+    bio.classList.remove("not-available-dark");
+  }
+
+  // set details
+  detailIcons.forEach((icon) => {
+    let el = document.querySelector(`.${icon.dataset.detailCategory}`);
+    // check if null, set text & color
+    if (nullItemsArr.includes(icon.dataset.detailCategory)) {
+      el.textContent = "Not available";
+      if (lightModeImg.classList.contains("dark-sun")) {
+        el.classList.add("not-available-dark");
+        el.classList.remove("not-available-light");
+        icon.src = `assets/icon-${icon.dataset.detailCategory}-dark-NA.svg`;
+      } else {
+        el.classList.add("not-available-light");
+        el.classList.remove("not-available-dark");
+        icon.src = `assets/icon-${icon.dataset.detailCategory}-light-NA.svg`;
+      }
+    } else {
+      // data is available
+      el.classList.remove("not-available-light");
+      el.classList.remove("not-available-dark");
+      // set icon & text color
+      if (lightModeImg.classList.contains("dark-sun")) {
+        icon.src = `assets/icon-${icon.dataset.detailCategory}-dark.svg`;
+        el.classList.add("dark-white-text");
+      } else {
+        icon.src = `assets/icon-${icon.dataset.detailCategory}.svg`;
+        el.classList.remove("dark-white-text");
+      }
+    }
+  });
+}
+
 function searchUser(e) {
   e.preventDefault();
+  // reset nullItems
+  nullItemsArr = [];
+
   const API = new FetchWrapper(`https://api.github.com/users/`);
   // show octocat on load & if search empty
   const user = searchInput.value === "" ? "octocat" : searchInput.value;
@@ -53,6 +122,7 @@ function searchUser(e) {
       error.style.display = "none";
     }
 
+    // img
     userImg.src = data.avatar_url;
 
     // show username for name if null
@@ -62,7 +132,7 @@ function searchUser(e) {
     } else {
       fullName.textContent = data.name;
     }
-
+    // username
     userName.textContent = `@${data.login}`;
 
     // format joined date
@@ -77,19 +147,19 @@ function searchUser(e) {
         month = "Feb";
         break;
       case 03:
-        month = "March";
+        month = "Mar";
         break;
       case 04:
-        month = "April";
+        month = "Apr";
         break;
       case 05:
         month = "May";
         break;
       case 06:
-        month = "June";
+        month = "Jun";
         break;
       case 07:
-        month = "July";
+        month = "Jul";
         break;
       case 08:
         month = "Aug";
@@ -111,75 +181,50 @@ function searchUser(e) {
     }
     dateJoined.textContent = `Joined ${day} ${month} ${year}`;
 
-    // bio validate
-    const bioText = data.bio === null ? "This profile has no bio" : data.bio;
-    bio.textContent = bioText;
-    if (data.bio === null) {
-      bio.classList.add("not-available-light");
-    } else {
-      bio.classList.remove("not-available-light");
-      bio.classList.remove("not-available-dark");
-    }
-
     // stat box
     repos.textContent = data.public_repos;
     followers.textContent = data.followers;
     following.textContent = data.following;
 
-    // find null details
-    const nullIcons = [];
-    for (const key in data) {
-      if (key === "location" && data[key] === null) {
-        nullIcons.push(key);
-      }
-      if (key === "blog" && data[key] === "") {
-        nullIcons.push(key);
-      }
-      if (key === "twitter_username" && data[key] === null) {
-        nullIcons.push(key);
-      }
-      if (key === "company" && data[key] === null) {
-        nullIcons.push(key);
-      }
-    }
+    // bio
+    const bioText = data.bio === null ? "This profile has no bio" : data.bio;
+    bio.textContent = bioText;
 
-    // set icons/text/color for N/A details
-    // change if dark mode
-    // const darkModeOn = lightModeImg.classList.contains("dark-sun");
-
+    // details
     detailIcons.forEach((icon) => {
-      let el = document.querySelector(`.${icon.dataset.detailCategory}`);
-      if (nullIcons.includes(icon.dataset.detailCategory)) {
-        el.classList.add("not-available-light");
-        el.textContent = "Not available";
-        icon.src = `assets/icon-${icon.dataset.detailCategory}-light-NA.svg`;
-      } else {
-        el.classList.remove("not-available-light");
-        icon.src = `assets/icon-${icon.dataset.detailCategory}.svg`;
-
-        // format blog, company, twitter
-        if (icon.dataset.detailCategory === "company") {
-          if (data.company.startsWith("@")) {
-            data.company = data.company.slice(1);
-          }
-          const url = `https://github.com/${data.company}`;
-          el.innerHTML = `<a href="${url}">@${data.company}</a>`;
-        } else if (icon.dataset.detailCategory === "twitter_username") {
-          const url = `https://twitter.com/${data.twitter_username}`;
-          el.innerHTML = `<a href="${url}">@${data.twitter_username}</a>`;
-        } else if (icon.dataset.detailCategory === "blog") {
-          el.innerHTML = `<a href="${data.blog}">${data.blog}</a>`;
-        } else {
-          el.textContent = data[icon.dataset.detailCategory];
+      // get detail text
+      let detailText = document.querySelector(
+        `.${icon.dataset.detailCategory}`
+      );
+      // format blog, company, twitter
+      if (icon.dataset.detailCategory === "company") {
+        if (data.company?.startsWith("@")) {
+          data.company = data.company.slice(1);
         }
+        const url = `https://github.com/${data.company}`;
+        detailText.textContent = `@${data.company}`;
+        detailText.setAttribute("href", url);
+      } else if (icon.dataset.detailCategory === "twitter_username") {
+        const url = `https://twitter.com/${data.twitter_username}`;
+        detailText.textContent = `@${data.twitter_username}`;
+        detailText.setAttribute("href", url);
+      } else if (icon.dataset.detailCategory === "blog") {
+        detailText.textContent = `${data.blog}`;
+        detailText.setAttribute("href", data.blog);
+      } else if (icon.dataset.detailCategory === "location") {
+        detailText.textContent = data[icon.dataset.detailCategory];
       }
     });
-
+    // set null items
+    getNA(data);
+    // format NA/A data
+    formatDetails();
     // clear input
     searchInput.value = "";
   });
 }
 
+// search event
 searchContainer.addEventListener("submit", searchUser);
 
 // toggle light mode colors on hover
@@ -205,7 +250,7 @@ function removeActiveClass(e) {
   }
 }
 
-// change text-img on light mode container
+// change light mode
 function switchMode() {
   // toggle dark mode classes
   lightModeImg.classList.toggle("dark-sun");
@@ -222,35 +267,12 @@ function switchMode() {
   bio.classList.toggle("dark-white-text");
   statText.forEach((el) => el.classList.toggle("dark-white-text"));
   statNum.forEach((el) => el.classList.toggle("dark-white-text"));
-  detailIcons.forEach(
-    (el) => (el.src = `assets/icon-${el.dataset.detailCategory}-dark.svg`)
-  );
-  detailText.forEach((el) => el.classList.toggle("dark-white-text"));
-
+  // format na/a items
+  formatDetails();
   // check if in dark & change light mode text/icon
   if (lightModeImg.classList.contains("dark-sun")) {
     lightModeText.textContent = "LIGHT";
     lightModeText.classList.add("dark-white-text");
-
-    // if bio is NA change text color & content
-    if (bio.textContent === "This profile has no bio") {
-      bio.classList.add("not-available-dark");
-    }
-
-    // change NA icons/text
-    // const detailsNA = detailText.filter(
-    //   (el) => el.textContent === "Not available"
-    // );
-
-    // detailsNA.forEach((el) => el.classList.add("not-available-dark"));
-
-    // detailIcons.forEach((el) => {
-    //   detailsNA.forEach((p) => {
-    //     if (p.classList.contains(el.dataset.detailCategory)) {
-    //       el.src = `assets/icon-${el.dataset.detailCategory}-dark-NA.svg`;
-    //     }
-    //   });
-    // });
   } else {
     lightModeText.textContent = "DARK";
     lightModeText.classList.remove("dark-white-text");
@@ -259,10 +281,11 @@ function switchMode() {
   }
 }
 
+// hover events for light mode container
 lightModeContainer.addEventListener("mouseover", addActiveClass);
-
 lightModeContainer.addEventListener("mouseout", removeActiveClass);
-
+// change light mode
 lightModeContainer.addEventListener("click", switchMode);
 
+// set octocat when loaded
 window.addEventListener("load", searchUser);
